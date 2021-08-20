f2875e61eb825f817be86673ed0d9a0b4929a010
alpha-v <alphaverta@gmail.com>
2021-08-20 21:36:02 +0200

    Implement packet parsing & filtering on client side

--- helpers.py
+++ helpers.py
@@ -47,6 +47,45 @@ async def get_measurement(ser):
 
 
 # START OF V2 CONNECTION/MEASUREMENT
+class DataPacket:
+    """
+    Helper class for storing information about the various data sources / how to decode them.
+    Used for packet filtering/easier access to the variables
+    """
+    def __init__(self, pkt_id, fmt, nt_fields, name_override=None):
+        """
+        Creates a new packet specification that can be registered with a data link.
+
+        :param fmt: The struct packing format string (less any endianness/encoding specifiers - those are added by the
+        data link)
+        :param nt_fields: A space-delimited string of fields passed to the named tuple
+        :param name_override: Optional override to change the namedtuple's name
+        """
+        self.pkt_id = pkt_id
+        self.fmt = fmt
+        self.named_tuple = namedtuple(name_override if name_override else DataPacket.__name__, nt_fields)
+
+    def get_id(self):
+        return self.pkt_id
+
+    def get_size(self):
+        return struct.calcsize(self.fmt)
+
+    def data_to_obj(self, data):
+        # Converts the passed in data into an instance of the tuple, or returns None if this fails
+        if len(data) != self.get_size():
+            # Size mismatch!
+            return None
+
+        return self.named_tuple._make(struct.unpack(self.fmt, data))
+
+
+# Define the data packets currently implemented
+accel_data_packet = DataPacket(0, 'fff', 'x y z', name_override='AccelData')
+gyro_data_packet = DataPacket(1, 'fff', 'gx gy gz', name_override='GyroData')
+mag_data_packet = DataPacket(2, 'fff', 'x y z', name_override='MagData')
+
+
 class DataLink:
     """
     New and improved class-based system for receiving telemetry.
@@ -54,7 +93,23 @@ class DataLink:
     Implements "V2"/first generation binary protocol
     """
 
-    def __init__(self):
+    def __init__(self, packet_filter=None, raw_mode=False):
+        """
+        Creates (but does not connect) a new data link.
+
+        NOTE: Packet filtering is applied on the client - server will still send out everything
+
+        :param packet_filter: Specifies a list of packet definitions to listen for / parse. Only enabled when raw_mode is False.
+        :param raw_mode: Whether or not to return the "raw" data.
+        """
+        # Keep track of the inputted params; convert the packet filter data into a dict to make it easier + more
+        # efficient to match to incoming packets
+        self.raw_mode = raw_mode
+        self.packet_filter = {}
+        if packet_filter is not None:
+            for packet in packet_filter:
+                self.packet_filter[packet.get_id()] = packet
+
         # Sync pattern used between packets
         self.syncpattern = b'\xFF\x8C'
 
@@ -88,7 +143,9 @@ class DataLink:
         """
         Attempts to read a packet from the stream, and will return None if it fails.
 
-        :return: A tuple containing (pkt_id, payload_data) if successful, else None
+        :return: Two cases:
+            [raw_mode=False]: The parsed packet struct; stream is re-tried until we find a matching entry
+            [raw_mode=True]: A tuple containing (pkt_id, payload_data) if successful, else None
         """
         # Read until the (next) sync pattern / discard any (irrelevant) data
         sync_data = await self.reader.readuntil(self.syncpattern)
@@ -96,9 +153,6 @@ class DataLink:
         # Read the header
         header_data = await self.reader.readexactly(struct.calcsize(self.system_format + self.header_fmt))
         header = self.HeaderType._make(struct.unpack(self.system_format + self.header_fmt, header_data))
-
-        # TODO: Implement packet ID validation? (or filtering, in which case we just skip to the next sync pattern /
-        #  keep doing that until we hit a packet that matches something in the filter
 
         # Read the payload
         payload_data = await self.reader.readexactly(header.payload_size)
@@ -121,20 +175,32 @@ class DataLink:
             print(f"Read Payload Size: {len(payload_data)}")
             return None
 
-        # Return the data and packet ID
-        # TODO: If we implement filtering, we can set up a class with packet definitions that automatically turn it into
-        #  a tuple
-        return header.pkt_id, payload_data
+        # Return the data and packet ID if we're currently in RAW mode, else try to parse it / retry the measurement
+        # / go to the next packet if it doesn't match the filter or is otherwise invalid
+        if self.raw_mode:
+            return header.pkt_id, payload_data
+
+        packet_definition = self.packet_filter.get(header.pkt_id)
+        if packet_definition:
+            parsed_packet = packet_definition.data_to_obj(payload_data)
+            if parsed_packet is not None:
+                return parsed_packet
+
+        # Unable to parse packet (some form of parsing error or it doesn't match the filter) - re-try the measurement
+        # [using recursion because goto doesn't exist/this is easier than a loop]
+        # TODO: Implement some form of maximum layers / exhaustion case? We don't want to have this go on "forever"
+        #  (or until the max. recursion depth is reached)
+        return await self.get_measurement()
 
 
 # DataLink usage example / test
 async def run_example():
-    test_link = DataLink()
+    test_link = DataLink(packet_filter=[accel_data_packet,  mag_data_packet], raw_mode=False)
     await test_link.connect()
 
     while True:
         measurement = await test_link.get_measurement()
-        #print(measurement)
+        print(measurement)
 
 
 if __name__ == '__main__':
